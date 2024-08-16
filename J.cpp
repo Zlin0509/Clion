c623f78d52acacdd24356af8fccdf0b737893613
zlin <2768228421@qq.com>
2024-08-16 16:16:27 +0800

    321

---
+++ J.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Zlin on 2024/8/16.
+//
+#include"bits/stdc++.h"
+
+using namespace std;
+
+typedef long long ll;
+const int N = 1e6 + 5;
+
+inline void Zlin() {
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int ttt;
+    cin >> ttt;
+    while (ttt--)Zlin();
+}
