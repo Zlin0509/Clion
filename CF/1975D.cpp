6c7ce9681abfc1fa0b2699b5aa8fe0954e2357aa
zlin <2768228421@qq.com>
2024-08-31 15:12:08 +0800

    321

---
+++ CF/1975D.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Zlin on 2024/8/31.
+//
+
+#include "bits/stdc++.h"
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+
+inline void Zlin() {
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    int ttt;
+    cin >> ttt;
+    while (ttt--) Zlin();
+}
