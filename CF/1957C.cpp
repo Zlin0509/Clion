0da791b2909271c1afcdc0449a9c8fb74850f009
zlin <2768228421@qq.com>
2024-09-02 21:02:16 +0800

    321

--- CF/1957C.cpp
+++ CF/1957C.cpp
@@ -10,8 +10,24 @@ typedef unsigned long long ull;
 typedef vector<int> vi;
 typedef vector<long long> vll;
 
-inline void Zlin() {
+const int N = 3e5 + 5;
+const ll mo = 1e9 + 7;
 
+ll sum[N];
+
+inline void Zlin() {
+    int n, k;
+    cin >> n >> k;
+    vi a(n + 1);
+    for (int i = 1, x, y; i <= k; i++) {
+        cin >> x >> y;
+        if (!a[x])--n;
+        ++a[x];
+        if (!a[y])--n;
+        ++a[y];
+    }
+//    cout << n << ' ';
+    cout << sum[n] << '\n';
 }
 
 int main() {
@@ -19,5 +35,8 @@ int main() {
     cin.tie(nullptr), cout.tie(nullptr);
     int ttt;
     cin >> ttt;
+    sum[1] = sum[0] = 1;
+    for (int i = 2; i <= N - 5; i++)
+        sum[i] = (sum[i - 1] + 2 * (i - 1) * sum[i - 2] % mo) % mo;
     while (ttt--) Zlin();
 }
