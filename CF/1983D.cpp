02102cf6b1cee1d27fcf9d951fd9b6f18cb83c9e
zlin <2768228421@qq.com>
2024-09-03 19:46:01 +0800

    321

---
+++ CF/1983D.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Zlin on 2024/9/3.
+//
+
+#include "bits/stdc++.h"
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+
+inline void Zlin() {
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    int ttt;
+    cin >> ttt;
+    while (ttt--) Zlin();
+}
