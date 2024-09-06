a8f306bdc296f60993ff506b632dd5f72684e3d6
zlin <2768228421@qq.com>
2024-09-06 10:40:30 +0800

    弱智题目

--- CF/1968G1.cpp
+++ CF/1968G1.cpp
@@ -12,13 +12,38 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
+const ll Base = 233, Mod = 3143271;
+const int N = 2e5 + 5;
+
+ll hd[N], Hash[N];
+char a[N];
+int n, l, r, cnt, ans;
+vi place;
+
+inline void init() {
+    place.clear();
+    ans = 0;
+}
+
 inline void Zlin() {
+    cin >> n >> cnt >> r;
+
+    init();
+
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        Hash[i] = (Hash[i - 1] * Base + a[i]) % Mod;
+    }
 
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr), cout.tie(nullptr);
+
+    hd[0] = 1;
+    for (int i = 1; i < N; i++) hd[i] = hd[i - 1] * Base % Mod;
+
     int ttt;
     cin >> ttt;
     while (ttt--) Zlin();
