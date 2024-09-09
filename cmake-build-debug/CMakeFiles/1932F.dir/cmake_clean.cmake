adbaf6d870aaac6104fbd68503e0904aa58f2e23
zlin <2768228421@qq.com>
2024-09-09 16:42:31 +0800

    123

---
+++ cmake-build-debug/CMakeFiles/1932F.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "1932F"
+  "1932F.pdb"
+  "CMakeFiles/1932F.dir/CF/1932F.cpp.o"
+  "CMakeFiles/1932F.dir/CF/1932F.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1932F.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
