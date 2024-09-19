38800772291b92462a3ff5e6ac6ce0b2cc4262d7
zlin <2768228421@qq.com>
2024-09-19 20:01:05 +0800

    9.19

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Clion
+Zlin在Mac平台上的做题记录
---
+++ _CF/2007F.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Zlin on 2024/9/19.
+//
+
+#include "bits/stdc++.h"
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+const int N = 4e5 + 5;
+int n, a[N];
+
+inline void Zlin() {
+    cin >> n;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    int ttt;
+    cin >> ttt;
+    while (ttt--) Zlin();
+}
