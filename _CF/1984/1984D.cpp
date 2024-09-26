4e97af91a1913dfca42813eaf1f08165e4d46c91
zlin <2768228421@qq.com>
2024-09-26 15:20:45 +0800

    9.26

--- _CF/1984/1984D.cpp
+++ _CF/1984/1984D.cpp
@@ -14,7 +14,43 @@ typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
 inline void Zlin() {
-
+    string s;
+    cin >> s;
+    int n = s.length();
+    if (count(s.begin(), s.end(), 'a') == n) {
+        cout << n - 1 << '\n';
+        return;
+    }
+    vector<int> a;
+    for (int i = 0; i < n; ++i) {
+        if (s[i] != 'a') {
+            a.push_back(i);
+        }
+    }
+    int m = a.size();
+    ll ans = 0;
+    for (int i = 1; i <= m; ++i) {
+        if (m % i)continue;
+        int ok = 1;
+        for (int j = i; j < m; ++j) {
+            int o = j % i;
+            if (s[a[j]] != s[a[o]] || (o && a[o] - a[o - 1] != a[j] - a[j - 1])) {
+                ok = 0;
+                break;
+            }
+        }
+        if (ok) {
+            int mi = n;
+            for (int j = i; j < m; j += i) {
+                mi = min(mi, a[j] - a[j - 1] - 1);
+            }
+            int r = n - a.back() - 1;
+            for (int l = 0; l <= a[0]; ++l) {
+                ans += max(0, min(r + 1, mi - l + 1));
+            }
+        }
+    }
+    cout << ans << '\n';
 }
 
 int main() {
