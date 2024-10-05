38f61c98803a05e4bf7fbef652d4b3e72c08691b
zlin <2768228421@qq.com>
2024-10-05 15:21:38 +0800

    10，5

--- _CF/GYM/The 2024 ICPC Kunming Invitational Contest/L.cpp
+++ _CF/GYM/The 2024 ICPC Kunming Invitational Contest/L.cpp
@@ -15,8 +15,32 @@ typedef vector<long long> vll;
 typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
-inline void Zlin() {
+struct node {
+    int x, y;
 
+    bool operator<(const node &a) const {
+        if (x != a.x) return x < a.x;
+        return y > a.y;
+    }
+};
+
+inline void Zlin() {
+    int n, p, q;
+    cin >> n >> p >> q;
+    vector<node> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i].x >> a[i].y;
+    sort(a.begin(), a.end());
+    ll ans = (ll) (q + 1) * (p + 1) * (q + p) / 2ll;
+    vi ly(n + 1);
+    for (int i = 0; i <= n; i++) ly[i] = q;
+    ly[0] = -1;
+    for (auto it: a) {
+        if (it.x >= p || it.y >= q) continue;
+        int t = lower_bound(ly.begin(), ly.end(), it.y) - ly.begin();
+        ans -= (ll) (p - it.x) * (ly[t] - it.y);
+        ly[t] = it.y;
+    }
+    cout << ans << '\n';
 }
 
 int main() {
