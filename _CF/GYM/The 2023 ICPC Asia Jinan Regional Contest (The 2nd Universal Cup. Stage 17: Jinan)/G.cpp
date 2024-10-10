858733133d38f0730bac8640326416e18e6e0417
zlin <2768228421@qq.com>
2024-10-10 10:39:27 +0800

    10,10

--- _CF/GYM/The 2023 ICPC Asia Jinan Regional Contest (The 2nd Universal Cup. Stage 17: Jinan)/G.cpp
+++ _CF/GYM/The 2023 ICPC Asia Jinan Regional Contest (The 2nd Universal Cup. Stage 17: Jinan)/G.cpp
@@ -16,6 +16,10 @@ typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
 const ll mo = 1e9 + 7;
+const int N = 1e6;
+
+vi f(N * 2 + 5);
+int cnt;
 
 ll qpw(ll u, ll v) {
     ll res = 1;
@@ -27,25 +31,58 @@ ll qpw(ll u, ll v) {
     return res;
 }
 
+int find(int u) {
+    return f[u] == u ? u : f[u] = find(f[u]);
+}
 
+void merge(int x, int y) {
+    int fx = find(x), fy = find(y);
+    if (f[fx] == f[fy]) return;
+    --cnt;
+    f[fx] = fy;
+}
 
 inline void Zlin() {
-    int r, c, cnt = 0;
+    int r, c;
     cin >> r >> c;
-    vi vis(r << 1 + 2), tag(c << 1 + 1);
-    vector<vector<int>> have(r + 1), place(c + 1);
+    vi vis(c + 1);
+    vector<vector<char>> a(r * 2 + 1, vector<char>(c + 1));
+    vector<vector<int>> have(c + 1);
     for (int i = 1; i <= r; i++) {
-        char x;
         for (int j = 1; j <= c; j++) {
-            cin >> x;
-            if (x == '1') {
-                place[j].push_back(i);
-                have[i].push_back(j);
+            cin >> a[i][j];
+            a[i + r][c - j + 1] = a[i][j];
+            if (a[i][j] == '1') {
+                have[j].push_back(i);
+                have[c - j + 1].push_back(i + r);
             }
         }
     }
-
-    cout << qpw(2, cnt) << '\n';
+    for (int i = 1; i <= c; i++) {
+        if (have[i].size() > 2) {
+            cout << 0 << '\n';
+            return;
+        }
+    }
+    cnt = r * 2;
+    for (int i = 1; i <= cnt; i++) f[i] = i;
+    for (int i = 1; i <= r * 2; i++) {
+        for (int j = 1; j <= c; j++) {
+            if (a[i][j] == '0') continue;
+            for (auto k: have[c - j + 1]) {
+                if (k == i + r || i == k + r) continue;
+                merge(i, k);
+            }
+        }
+    }
+    for (int i = 1; i <= r; i++) {
+        if (find(i) == find(i + r)) {
+            cout << 0 << '\n';
+            return;
+        }
+    }
+    ll ans = qpw(2, cnt / 2);
+    cout << ans << '\n';
 }
 
 int main() {
